cbec03e08c7096cf0ada655cd3283971f20d1700
Emre Bozkurt <emre75kurt@gmail.com>
2022-02-01 11:43:37 +0300

    Initial Release

---
+++ app/src/main/java/com/emrebozkurt/wordle/MainActivity.java
@@ -0,0 +1,39 @@
+package com.emrebozkurt.wordle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String url = "https://f.github.io/wordle-tr/";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+        webView.loadUrl(url);
+    }
+}
